b04e9bb50ef12cf91247265c4cb842e3cb191266
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2022-12-15 14:31:33 +0900

    update

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -2,41 +2,41 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-20">
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-20 dark:bg-slate-800">
       <h1 className="text-3xl font-bold">Stable Diffusion web UI</h1>
       <ul className="flex justify-center items-center flex-wrap -mb-px">
       <li className="mr-2">
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500" href="/" aria-current="page">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500" href="/" aria-current="page">
             Home
           </Link>
         </li>
       <li className="mr-2">
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500" href="/txt2img">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500" href="/txt2img">
             Txt2Img
           </Link>
         </li>
         <li>
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500"  href="/extras">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500"  href="/extras">
             Extras
           </Link>
         </li>
         <li>
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500"  href="/png_info">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500"  href="/png_info">
             PNG Info
           </Link>
         </li>
         <li>
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500"  href="/checkpoint_merger">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500"  href="/checkpoint_merger">
             Checkopoint Merger
           </Link>
         </li>
         <li>
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500"  href="/textual_inversion">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500"  href="/textual_inversion">
             Textual Inversion
           </Link>
         </li>
         <li>
-          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-purple-500"  href="/settings">
+          <Link className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-400 hover:border-purple-500"  href="/settings">
             Settings
           </Link>
         </li>
